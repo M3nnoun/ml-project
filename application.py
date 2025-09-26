36b93375b6929529ecce5a7869532f44456f3c39
m3nnoun <moulshe@protonmail.com>
2025-09-27 00:48:32 +0100

    rename the main flask file to application.py

--- application.py
+++ application.py
@@ -1,18 +1,21 @@
-from flask import Flask , request , render_template
+from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-from src.pipeline.predict_pipeline import CustomData ,PredictPipeline 
+app=Flask(__name__)
 
-application=Flask(__name__)
+#app=application
 
-@application.route('/')
+## Route for a home page
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') 
 
-@application.route('/predicte',methods=['GET','POST'])
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -27,13 +30,17 @@ def predict_datapoint():
             writing_score=float(request.form.get('reading_score'))
 
         )
-        data_df=data.get_data_as_data_frame()
-        predict_obj=PredictPipeline()
-        prediction=predict_obj.predict(data_df)
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
 
-        return render_template('home.html',results=prediction[0])
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
     
 
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
 
-if __name__=='__main__':
-    application.run(host='0.0.0.0')
