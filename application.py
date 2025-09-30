f517ee5a2d0f126dc6577771b237ddb62b835c6c
m3nnoun <moulshe@protonmail.com>
2025-09-30 15:30:37 +0100

    make a small chnage on the predict endpoint

--- application.py
+++ application.py
@@ -15,7 +15,7 @@ app=Flask(__name__)
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predict',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
